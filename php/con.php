b2f078d2d58d016c126215497b1d1aca7f434f89
GustavoMSevero <gustavo.msevero@gmail.com>
2025-02-01 10:32:56 -0300

    Alterada arquivo de conexão do banco.

--- php/con.php
+++ php/con.php
@@ -4,8 +4,8 @@ function conectar(){
 	
 	try {
 		$opcoes = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION);
-		$pdo = new PDO("mysql:host=localhost;dbname=react-crud;", "root", "root", $opcoes);
-		// $pdo = new PDO("mysql:host=localhost;dbname=custojoin_react-crud;", "custojoi_admin", "Admin@2022", $opcoes);
+		// $pdo = new PDO("mysql:host=localhost;dbname=react-crud;", "root", "root", $opcoes);
+		$pdo = new PDO("mysql:host=localhost;dbname=custojoin_react-crud;", "custojoi_admin", "Admin@2022", $opcoes);
 	} catch (Exception $e) {
 		echo $e->getMessage();
 	}
